313b1ecb940b530f98d50a1ebc74c92a69ee024b
EthanMorinUC <morinej@mail.uc.edu>
2024-10-07 23:01:24 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import jdk.dynalink.StandardNamespace;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What age are you?");
+        int age = input.nextInt();
+        if (age >= 21)
+            System.out.println("You get a paper wrist band");
+        else
+            System.out.println("You don't get a paper wrist band");
     }
 }
